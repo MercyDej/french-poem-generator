2d1122fcc1f5d6282b3af31f2065f97bd5181e09
mercymih <mihretJesus@yahoo.com>
2024-09-03 16:54:55 +0300

    Added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-//alert("God Knows!");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Demain, dès l’aube, à l’heure où blanchit la campagne",
+    autoStart: true,
+    cursor: "",
+    delay: 10,
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
