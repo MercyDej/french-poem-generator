6a4c64c8c146a64a07f7f4d182d82bb5b84f2f84
mercymih <mihretJesus@yahoo.com>
2024-09-04 17:15:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 function displayPoem(response) {
+  console.log(response.data.answer);
   console.log("Poem generated!");
 
   new Typewriter("#poem", {
@@ -17,7 +18,7 @@ function generatePoem(event) {
   let apiKey = "c71e44b46oa0t62f8d332460f8567af3";
 
   let context =
-    "You are a Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a French poem about ${userInstructions.value}`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
